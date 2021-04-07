fe7e885be2aed5713ba7a7bc4ddc534c5e15ce51
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2021-04-06 22:39:39 -0300

    Implementação Web Static Files

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,16 @@
 package main
 
-import "gocrudperson/internal/api"
+import (
+	"gocrudperson/internal/api"
+	"log"
+)
 
-const apiAddr string = ":8080"
+const (
+	version        = "0.0.1-alpha.0"
+	apiAddr string = ":8080"
+)
 
 func main() {
+	log.Printf("go-crud-person %s (powered by guilhermerodrigues680).", version)
 	api.Listen(apiAddr)
 }
--- internal/api/server.go
+++ internal/api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"gocrudperson/internal/api/controller"
 	"gocrudperson/internal/api/middleware"
@@ -10,13 +12,35 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func getWebDir() (string, error) {
+	ex, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+
+	exPath := filepath.Dir(ex)
+	dir, err := filepath.Abs(exPath + "/../web")
+	if err != nil {
+		return "", err
+	}
+
+	return dir, nil
+}
+
 func Listen(addr string) {
 	r := mux.NewRouter()
 
+	dir, err := getWebDir()
+	if err != nil {
+		panic(err)
+	}
+
 	r.Use(middleware.LoggingMiddleware)
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir(dir)))
 	r.HandleFunc("/api/v1", controller.RootHandler).Methods(http.MethodGet)
 	controller.PersonRouter(r.PathPrefix("/api/v1/person").Subrouter())
 
-	log.Println("go-crud-person. Listening on: " + addr)
+	log.Printf("Web static dir: %s\n", dir)
+	log.Printf("Listening on: %s\n", addr)
 	log.Fatal(http.ListenAndServe(addr, r))
 }
